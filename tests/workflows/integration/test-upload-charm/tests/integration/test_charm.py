d62b6c066ac212b2c826e88bc8fdf491ee95476d
Weii Wang <weii.wang@canonical.com>
2023-03-01 19:52:24 +0800

    asd

--- tests/workflows/integration/test-upload-charm/tests/integration/test_charm.py
+++ tests/workflows/integration/test-upload-charm/tests/integration/test_charm.py
@@ -1,32 +1,19 @@
-#!/usr/bin/env python3
-# Copyright 2023 Weii Wang
+# Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import asyncio
-import logging
-from pathlib import Path
-
-import pytest
-import yaml
 from pytest_operator.plugin import OpsTest
-
-logger = logging.getLogger(__name__)
-
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
-APP_NAME = METADATA["name"]
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest):
+async def test_build_and_deploy(ops_test: OpsTest, pytestconfig):
     """Build the charm-under-test and deploy it together with related charms.
 
     Assert on the unit status before any relations/configurations take place.
     """
-    # Build and deploy charm from local source folder
+    APP_NAME = "test"
     charm = await ops_test.build_charm(".")
-    resources = {"httpbin-image": METADATA["resources"]["httpbin-image"]["upstream-source"]}
+    resources = {"test-image": pytestconfig.getoption("--test-image")}
 
-    # Deploy the charm and wait for active/idle status
     await asyncio.gather(
         ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME),
         ops_test.model.wait_for_idle(
