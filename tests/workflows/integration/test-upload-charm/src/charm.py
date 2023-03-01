021858431c0d8bdf2abf37b022c6ae8555c238ad
Weii Wang <weii.wang@canonical.com>
2023-03-01 20:07:17 +0800

    asdsad

--- tests/workflows/integration/test-upload-charm/src/charm.py
+++ tests/workflows/integration/test-upload-charm/src/charm.py
@@ -1,6 +1,11 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Dummy charm for tesing charm upload."""
+
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
+from ops.model import ActiveStatus
 
 
 class TestUploadCharm(CharmBase):
@@ -9,6 +14,7 @@ class TestUploadCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         self.unit.status = ActiveStatus()
+
 
 if __name__ == "__main__":  # pragma: nocover
     main(TestUploadCharm)
--- tests/workflows/integration/test-upload-charm/tests/integration/test_charm.py
+++ tests/workflows/integration/test-upload-charm/tests/integration/test_charm.py
@@ -1,22 +1,22 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
+import asyncio
 
 from pytest_operator.plugin import OpsTest
 
 
-@pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest, pytestconfig):
     """Build the charm-under-test and deploy it together with related charms.
 
     Assert on the unit status before any relations/configurations take place.
     """
-    APP_NAME = "test"
+    app_name = "test"
     charm = await ops_test.build_charm(".")
     resources = {"test-image": pytestconfig.getoption("--test-image")}
 
     await asyncio.gather(
-        ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME),
+        ops_test.model.deploy(charm, resources=resources, application_name=app_name),
         ops_test.model.wait_for_idle(
-            apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
+            apps=[app_name], status="active", raise_on_blocked=True, timeout=1000
         ),
     )
