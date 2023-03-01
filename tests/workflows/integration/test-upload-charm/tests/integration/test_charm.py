9d3a5f8c8583a830f9785468e76df0de6bfd276a
Weii Wang <weii.wang@canonical.com>
2023-03-01 21:12:50 +0800

    Update test_charm.py

--- tests/workflows/integration/test-upload-charm/tests/integration/test_charm.py
+++ tests/workflows/integration/test-upload-charm/tests/integration/test_charm.py
@@ -19,7 +19,9 @@ async def test_build_and_deploy(ops_test: OpsTest, pytestconfig):
     resources = {"test-image": pytestconfig.getoption("--test-image")}
 
     await asyncio.gather(
-        ops_test.model.deploy(charm, resources=resources, application_name=app_name),
+        ops_test.model.deploy(
+            charm, resources=resources, application_name=app_name, series="jammy"
+        ),
         ops_test.model.wait_for_idle(
             apps=[app_name], status="active", raise_on_blocked=True, timeout=1000
         ),
